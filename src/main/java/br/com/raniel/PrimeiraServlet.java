5b08fa95a0eb103a03a15e5f8c79d05af2caf1ed
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-04-12 22:30:19 -0300

    refactor: remove unused file and set PrimeiraServlet class to display on home(or "/")

--- src/main/java/br/com/raniel/PrimeiraServlet.java
+++ src/main/java/br/com/raniel/PrimeiraServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/servlet")
+@WebServlet("/")
 public class PrimeiraServlet extends HttpServlet {
 
     @Override
